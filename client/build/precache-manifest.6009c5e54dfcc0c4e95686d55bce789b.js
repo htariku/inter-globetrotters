404ac3c792ee4a387044594986d75edeb01bba47
htariku <hgtariku@yahoo.com>
2022-04-25 21:23:54 -0400

    skeleton files

---
+++ client/build/precache-manifest.6009c5e54dfcc0c4e95686d55bce789b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0725b3a321fa630847b02b9203d92b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cee886cd077810a44c9",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "7fbdcf8a5028cbfc532b",
+    "url": "/static/js/2.047d8caa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.047d8caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cee886cd077810a44c9",
+    "url": "/static/js/main.d80ee32f.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
